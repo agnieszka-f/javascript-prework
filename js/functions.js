06cf756817819f3442d160e69660f744d11ca48f
Agnieszka Falandys <agnfal@gmail.com>
2021-05-05 20:30:42 +0200

    zadanie do modułu 5.3

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+let playerMove = 'papier';
+let computerMove = 'nożyce';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
